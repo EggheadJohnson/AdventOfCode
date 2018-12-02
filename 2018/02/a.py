7cf9bfb153c79d06165db47fc81202bd8aa7b045
Paul Johnson <PaulE.Johnson@gmail.com>
2018-12-01 22:20:00 -0700

    2018 day 2 complete

--- 2018/02/a.py
+++ 2018/02/a.py
@@ -5,3 +5,29 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 input_path = file_path + "/a.in.txt"
 
 inpt = open(input_path, 'r')
+
+twos = 0
+threes = 0
+
+for line in inpt:
+    d = {}
+    line = line.strip()
+    for c in line:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    two_found = False
+    three_found = False
+    for c in d:
+        print c, d[c]
+        if d[c] == 2:
+            two_found = True
+        if d[c] == 3:
+            three_found = True
+    if two_found:
+        twos += 1
+    if three_found:
+        threes += 1
+
+print twos * threes
--- 2018/02/b.py
+++ 2018/02/b.py
@@ -2,6 +2,36 @@ import os
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 
-input_path = file_path + "/b.in.txt"
+input_path = file_path + "/a.in.txt"
 
 inpt = open(input_path, 'r')
+
+seen = []
+
+def compare(seen, line):
+    for comp in seen:
+        diffs = 0
+        for x in range(len(comp)):
+            if comp[x] != line[x]:
+                diffs += 1
+            if diffs > 1:
+                break
+        if diffs == 1:
+            return comp
+    return False
+
+def similars(line_a, line_b):
+    out = ''
+    for x in range(len(line_a)):
+        if line_a[x] == line_b[x]:
+            out += line_a[x]
+    return out
+
+for line in inpt:
+    line = line.strip()
+
+    result = compare(seen, line)
+    if result:
+        print similars(result, line)
+        break
+    seen.append(line)
